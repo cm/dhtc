fe9f9251cb78fa16c01196f8f98f9da3b9ce573a
Christian Burland Mankowski <chrisaburland@gmail.com>
2017-11-30 11:57:27 +0000

    Amended some naming.

--- examples/crawler.js
+++ examples/crawler.js
@@ -1,6 +1,6 @@
-const DHTC = require('../index')
+const DHTCrawler = require('../index')
 
-const crawler = new DHTC({ address: '0.0.0.0', port: 6881 })
+const crawler = new DHTCrawler({ address: '0.0.0.0', port: 6881 })
 
 crawler.on('infohash', (infohash, address, port) => {
   console.log(`${infohash} from ${address}:${port}`)
